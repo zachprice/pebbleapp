411fdd1ede71cd05a596c43199b7980e5caaec27
Zach Price <zachprice@purdue.edu>
2015-01-17 00:22:20 -0500

    Renamed Variables

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <pebble.h>
   
-static Window *s_main_window;
-static TextLayer *s_time_layer;
+static Window *window;
+static TextLayer *times;
+static TextLayer *date;
 
 static void update_time() {
   // Get a tm structure
@@ -21,30 +22,44 @@ static void update_time() {
   }
 
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(times, buffer);
+}
+
+static void update_date() {
+  
 }
 
 static void main_window_load(Window *window) {
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
+  times = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_background_color(times, GColorClear);
+  text_layer_set_text_color(times, GColorBlack);
+  text_layer_set_text(times, "00:00");
+  
+  // Create date TextLayer
+  date = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_background_color(date, GColorClear);
+  text_layer_set_text_color(date, GColorBlack);
+  text_layer_set_text(date, "00:00");
 
   // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_font(times, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(times, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(times));
   
   // Make sure the time is displayed from the start
   update_time();
+  
+  //Update date
+  update_date();
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
+  text_layer_destroy(times);
+  text_layer_destroy(date);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -53,16 +68,16 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   
 static void init() {
   // Create main Window element and assign to pointer
-  s_main_window = window_create();
+  window = window_create();
 
   // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
+  window_set_window_handlers(window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
 
   // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
+  window_stack_push(window, true);
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
@@ -70,7 +85,7 @@ static void init() {
 
 static void deinit() {
   // Destroy Window
-  window_destroy(s_main_window);
+  window_destroy(window);
 }
 
 int main(void) {
