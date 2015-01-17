e7e3e43238eba2db30101c99ec22780cde7b5136
Shane DeWael <dewaels@purdue.edu>
2015-01-17 16:42:44 -0500

    Google Calendar API integration and Settings page

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,18 @@
+var initialized = false;
+
+Pebble.addEventListener("ready", function() {
+  console.log("ready called!");
+  initialized = true;
+});
+
+Pebble.addEventListener("showConfiguration", function() {
+  console.log("showing configuration");
+  Pebble.openURL('http://shanedewael.com/simplecalset/index.html');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  console.log("configuration closed");
+  // webview closed
+  var options = JSON.parse(decodeURIComponent(e.response));
+  console.log("Options = " + JSON.stringify(options));
+});
