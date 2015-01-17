56412449c996c8714af771723f77010fe1cf095e
Shane DeWael <dewaels@purdue.edu>
2015-01-17 03:17:36 -0500

    Just stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <pebble.h>
   
 static Window *window;
+static Window *calendar;
 static TextLayer *times;
 static TextLayer *dates;
+static TextLayer *caltext;
 
 static void update_time() {
   // Get a tm structure
@@ -67,6 +69,19 @@ static void main_window_load(Window *window) {
   update_date();
 }
 
+static void cal_window_load(Window *calendar) {
+  caltext = text_layer_create(GRect(0, 0, 144, 144));
+  text_layer_set_background_color(caltext, GColorClear);
+  text_layer_set_text_color(caltext, GColorBlack);
+  text_layer_set_font(caltext, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(caltext, "Cliche lo-fi deep v, semiotics Williamsburg gastropub meggings farm-to-table. Bitters Williamsburg forage, chillwave Pinterest cornhole tilde chia migas yr irony PBR meditation. Cornhole mumblecore flannel, pour-over Pinterest wayfarers blog forage swag Helvetica DIY beard. Occupy vinyl leggings, flannel +1 VHS Helvetica chambray deep v cronut. Readymade yr pork belly, trust fund ennui polaroid raw denim. Brunch Schlitz Portland fingerstache, Williamsburg disrupt gentrify PBR&B mumblecore pork belly four dollar toast. Pinterest salvia raw denim 3 wolf moon pour-over, mustache direct trade American Apparel fap slow-carb hoodie.");
+  layer_add_child(window_get_root_layer(calendar), text_layer_get_layer(caltext));
+}
+
+static void cal_window_unload(Window *calendar) {
+  text_layer_destroy(caltext);
+}
+
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(times);
@@ -82,16 +97,25 @@ static void init() {
   window = window_create();
   window_set_background_color(window,GColorBlack);
 
+  calendar = window_create();
+
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
 
+  window_set_window_handlers(calendar, (WindowHandlers) {
+    .load = cal_window_load,
+    .unload = cal_window_unload
+  });
+
   window_set_fullscreen(window,true);
+  window_set_fullscreen(calendar,true);
 
   // Show the Window on the watch, with animated=true
   window_stack_push(window, true);
+  window_stack_push(calendar, true);
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
@@ -100,6 +124,7 @@ static void init() {
 static void deinit() {
   // Destroy Window
   window_destroy(window);
+  window_destroy(calendar);
 }
 
 int main(void) {
