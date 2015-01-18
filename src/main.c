fa2ead4bada99abb42a85e741d8e10a0d1b76afd
Zach Price <zachprice@purdue.edu>
2015-01-17 23:32:48 -0500

    Changed to watchface and re-did second layer

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
 #include <pebble.h>
 
-#define NUM_MENU_ITEMS 3
 #define TAP_NOT_DATA true
   
 static Window *window;
-static Window *event_list;
+static Window *weather;
 static TextLayer *times;
 static TextLayer *dates;
-static TextLayer *caltext;
-static MenuLayer *menu_layer;
-static TextLayer *static_time;
+static TextLayer *condition;
+static TextLayer *clocks;
+static TextLayer *day;
 
 int delta; 
 
@@ -22,61 +21,20 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
 
   // Determines if change was user-intended, and loads calendar
   if (delta > 400) {
-    if (!window_is_loaded(event_list)) {
-      window_stack_push(event_list, true);
+    if (!window_is_loaded(weather)) {
+      window_stack_push(weather, true);
     } else {
       window_stack_pop(true);
     }
   }
 }
 
-static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  return NUM_MENU_ITEMS;
-}
-
-static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
-  // Determine which section we're going to draw in
-    switch (cell_index->section) {
-    case 0:
-      // Use the row to specify which item we'll draw
-        switch (cell_index->row) {
-        case 0:
-          // Use to show data in each cell
-          menu_cell_basic_draw(ctx, cell_layer, "Cell 1 Title", "Subtitle", NULL);
-          break;
-        case 1:
-          menu_cell_basic_draw(ctx, cell_layer, "Cell 2 Title", "Subtitle", NULL); 
-          break;
-        case 2:
-          menu_cell_basic_draw(ctx, cell_layer, "Cell 3 Title", "Subtitle", NULL); 
-          break;
-          }
-      break;
-    }
-}
-
-
-
-// Here we capture when a user selects a menu item
-void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-  // Use the row to specify which item will receive the select action
-  switch (cell_index->row) {
-    // This is the menu item with the cycling icon
-    case 1:
-      // After changing the icon, mark the layer to have it updated
-      layer_mark_dirty(menu_layer_get_layer(menu_layer));
-      break;
-  }
-
-}
-
-
 
 // Select-button Handler 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Loads event list on button click
   if (window_is_loaded(window)) {
-    window_stack_push(event_list, true);
+    window_stack_push(weather, true);
   }
 }
 
@@ -107,15 +65,7 @@ static void update_time() {
 
 }
 
-static void update_date() {
-  time_t temp = time(NULL); 
-  static char buffer[] = "Loading text";
-  struct tm *tick_time = localtime(&temp);
-  strftime(buffer, sizeof(buffer), "%a. %d", tick_time);
-  text_layer_set_text(dates, buffer);
-}
-
-static void update_static_time() {
+static void update_weather_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
@@ -133,9 +83,17 @@ static void update_static_time() {
   }
 
   // Display this time on the TextLayer
-  text_layer_set_text(static_time, buffer);
+  text_layer_set_text(clocks, buffer);
+
 }
 
+static void update_date() {
+  time_t temp = time(NULL); 
+  static char buffer[] = "Loading text";
+  struct tm *tick_time = localtime(&temp);
+  strftime(buffer, sizeof(buffer), "%a. %d", tick_time);
+  text_layer_set_text(dates, buffer);
+}
 
 
 static void main_window_load(Window *window) {
@@ -180,46 +138,44 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(dates);
 }
 
-static void cal_window_load(Window *event_list) {
-  //The static time layer
-  static_time = text_layer_create(GRect(0, 125, 144, 55));
-  text_layer_set_background_color(static_time, GColorBlack);
-  text_layer_set_text_color(static_time, GColorWhite);
-  text_layer_set_text_alignment(static_time, GTextAlignmentCenter);
-  text_layer_set_font(static_time, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-  text_layer_set_text(static_time, "00:00");
+//load the weather/clock window
+static void weather_load(Window *weather) {
+  //Clock Layer
+  clocks = text_layer_create(GRect(0, 84, 144, 84));
+  text_layer_set_background_color(clocks, GColorWhite);
+  text_layer_set_text_color(clocks, GColorBlack);
+  text_layer_set_text(clocks, "Loading..");
+  
+  //Date Layer
+  day = text_layer_create(GRect(0, 84, 144, 84));
+  text_layer_set_background_color(day, GColorClear);
+  text_layer_set_text_color(day, GColorBlack);
+  text_layer_set_text(day, "Loading..");
+  
+  //Weather Layer
+  condition = text_layer_create(GRect(0, 0, 144, 84));
+  text_layer_set_background_color(condition, GColorBlack);
+  text_layer_set_text_color(condition, GColorWhite);
+  text_layer_set_text(condition, "Loading..");
   
   
-  Layer *window_layer = window_get_root_layer(event_list);
-  //GRect bounds = layer_get_frame(window_layer);
+  //Formatting text
+  text_layer_set_font(clocks, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(clocks, GTextAlignmentCenter);
 
-  // Create the menu layer
-  menu_layer = menu_layer_create(GRect(0, 0, 144, 125));
+  text_layer_set_font(day, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_alignment(day, GTextAlignmentRight);
   
-  // Set all the callbacks for the menu layer
-  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_rows = menu_get_num_rows_callback,
-    .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
-  });
-
-  // Bind the menu layer's click config provider to the window for interactivity
-  menu_layer_set_click_config_onto_window(menu_layer, event_list);
-
-  // Add it to the window for display
-  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
-  layer_add_child(window_get_root_layer(event_list), text_layer_get_layer(static_time));
+  //Insert the text_layers
+  layer_add_child(window_get_root_layer(weather), text_layer_get_layer(clocks));
+  layer_add_child(window_get_root_layer(weather), text_layer_get_layer(day));
+  layer_add_child(window_get_root_layer(weather), text_layer_get_layer(condition));
   
-  //Set the time correctly
-  update_static_time();
+  update_weather_time();
+}
+
+static void weather_unload(Window *weather) {
   
-  //Set the date
-
-} 
-
-static void cal_window_unload(Window *calendar) {
-  text_layer_destroy(caltext);
-  menu_layer_destroy(menu_layer);
 }
 
 
@@ -232,7 +188,7 @@ static void init() {
   window = window_create();
   window_set_background_color(window, GColorBlack);
 
-  event_list = window_create();
+  weather = window_create();
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(window, (WindowHandlers) {
@@ -240,14 +196,14 @@ static void init() {
     .unload = main_window_unload
   });
 
-  window_set_window_handlers(event_list, (WindowHandlers) {
-    .load = cal_window_load,
-    .unload = cal_window_unload
+  window_set_window_handlers(weather, (WindowHandlers) {
+    .load = weather_load,
+    .unload = weather_unload
   });
 
 
   window_set_fullscreen(window, true);
-  window_set_fullscreen(event_list, true);
+  window_set_fullscreen(weather, true);
 
   // Show the Window on the watch, with animated=true
   window_stack_push(window, true);
@@ -267,7 +223,7 @@ static void init() {
 static void deinit() {
   // Destroy Window
   window_destroy(window);
-  window_destroy(event_list);
+  window_destroy(weather);
   // Subscribe to the accelerometer data service
   // Choose update rate
   accel_data_service_unsubscribe();
